a7717335cb601a63aa5b5407f94d34e10dc6971a
no3utuff4uk <no3utuff4uk@gmail.com>
2017-03-06 15:03:45 +0300

    Небольшой рефакторинг кода

--- src/ru/no3utuff4uk/stribogGUI/StribogGUIMain.java
+++ src/ru/no3utuff4uk/stribogGUI/StribogGUIMain.java
@@ -4,8 +4,6 @@
 package ru.no3utuff4uk.stribogGUI;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
